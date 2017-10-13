74db77240a816945daaf2fbdc00c9bf938cda29b
CorTal <sygma666@hotmail.fr>
2017-10-13 16:03:55 +0200

    En fait j'avais oublié de remplir

--- fournisseur/models.py
+++ fournisseur/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Fournisseur(models.Model):
+
+	nom = models.CharField(max_length=30)
+	email = models.EmailField(max_length=100)
+	adresse = models.CharField(max_length=200)
+	code_postal = models.PositiveIntegerField()
+	ville = models.CharField(max_length=50)
+	produit = models.ManyToMany("produit.Produit")
