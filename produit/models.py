a9be7c2187aee14b298d733bc8990115ae85fe59
Alexis Leblond <alexis@MacBook-Pro-de-Alexis.local>
2017-11-14 11:51:43 +0100

    deploiement de maj

--- produit/models.py
+++ produit/models.py
@@ -3,11 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-<<<<<<< HEAD
-class Produit(models.Model):
-=======
 class Produit(models.Model): 
->>>>>>> 49118f825e27073741915e9d2d9d4299745029b0
     nom = models.CharField(max_length=30)
     prix = models.FloatField()
     descriptif = models.TextField()
