a3104018b103ab19c06f185ae3f804d4e5dba429
thomas <calatayud.thomas@hotmail.fr>
2017-11-14 11:14:37 +0100

    Modification

--- produit/models.py
+++ produit/models.py
@@ -4,18 +4,16 @@ from django.utils import timezone
 # Create your models here.
 
 class Produit(models.Model):
-	nom = models.CharField(max_length=30)
-	prix = models.FloatField()
-	descriptif = models.TextField()
-	qte = models.PositiveIntegerField()
-        image = models.ImageField(
-                blank=True, null=True)
-        published_date = models.DateTimeField(
-                blank=True, null=True)
+    nom = models.CharField(max_length=30)
+    prix = models.FloatField()
+    descriptif = models.TextField()
+    qte = models.PositiveIntegerField()
+    image = models.ImageField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-        def publish(self):
-            self.published_date = timezone.now()
-            self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-        def __str__(self):
-            return self.nom
+    def __str__(self):
+        return self.nom
