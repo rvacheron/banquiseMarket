efacd2af353dc26bd876e226fce90a0567b17277
thomas <calatayud.thomas@hotmail.fr>
2017-11-16 16:47:01 +0100

    Ajout des commentaires

--- produit/views.py
+++ produit/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Produit
+from .models import Produit, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -11,3 +12,16 @@ def produit_list(request):
 def produit_detail(request, pk):
     produit = get_object_or_404(Produit, pk=pk)
     return render(request, 'produit/produit_detail.html', {'produit': produit, 'nbar': 'produits'})
+
+def add_comment_to_produit(request, pk):
+    produit = get_object_or_404(Produit, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.produit = produit
+            comment.save()
+            return redirect('produit_detail', pk=produit.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'produit/add_comment_to_produit.html', {'form': form})
